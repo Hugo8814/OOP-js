093dc3c74a3dd05416a67a2665753ed1339ae449
hugo pereira <Hugopereira8814@gmail.com>
2023-09-05 22:50:47 +0100

    done, next

--- starter/script.js
+++ starter/script.js
@@ -1,29 +1,32 @@
 'use strict';
 
-// const Person = function (firstName, birthYear) {
-//   this.firstName = firstName;
-//   this.birthYear = birthYear;
+const Person = function (firstName, birthYear) {
+  this.firstName = firstName;
+  this.birthYear = birthYear;
 
-//   ////never do this
-//   //   this.calcAge = function () {
-//   //     console.log(2037 - this.birthYear);
-//   //   };
-// };
-// const hugo = new Person('hugo', 2003);
+  ////never do this
+  //   this.calcAge = function () {
+  //     console.log(2037 - this.birthYear);
+  //   };
+};
+const hugo2 = new Person('hugo', 2003);
 
-// console.log(hugo);
+const danny = new Person('danny', 2004);
+console.log(danny);
 
-// const danny = new Person('danny', 2004);
-// console.log(danny);
+console.log(hugo2 instanceof Person);
+console.log(Person.prototype);
 
-// console.log(hugo instanceof Person);
-// console.log(Person.prototype);
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.birthYear);
+};
 
-// Person.prototype.calcAge = function () {
-//   console.log(2037 - this.birthYear);
-// };
-
-// hugo.calcAge();
+hugo2.calcAge();
+Person.hey = function () {
+  console.log('hey');
+  console.log(this);
+};
+Person.hey();
 
 // console.log(hugo.__proto__);
 // console.log(hugo.__proto__ === Person.prototype);
@@ -100,10 +103,109 @@ const Personcl = class {
   calcAge() {
     console.log(2023 - this.birthYear);
   }
+  greet() {
+    console.log(`hey ${this.firstName}`);
+  }
+  get age() {
+    return 2023 - this.birthYear;
+  }
+  static hey() {
+    console.log('hey2');
+    console.log(this);
+  }
 };
 
 const hugo = new Personcl('hugo', 2004);
-console.log(hugo);
-hugo.calcAge();
+// console.log(hugo);
+// hugo.calcAge();
 
-console.log(hugo.__proto__ === Personcl.prototype);
+console.log(hugo.Age);
+
+// console.log(hugo.__proto__ === Personcl.prototype);
+// // Personcl.prototype.greet = function () {
+// //   console.log(`hey ${this.firstName}`);
+// // };
+// hugo.greet();
+
+const account = {
+  owner: 'hugo',
+  movement: [2444, 3434, 543, 45, 673],
+  get latest() {
+    return this.movement.slice(-1).pop();
+  },
+  set latest(mov) {
+    this.movement.push(mov);
+  },
+};
+
+console.log(account.latest);
+account.latest = 50;
+console.log(account.movement);
+
+const PersonProto = {
+  calcAge() {
+    console.log(2023 - this.birthYear);
+  },
+  init(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  },
+};
+const steven = Object.create(PersonProto);
+
+console.log(steven);
+steven.name = 'steven';
+steven.birthYear = 2003;
+
+steven.calcAge();
+
+console.log(steven.__proto__ === PersonProto);
+
+const sarah = Object.create(PersonProto);
+sarah.init('sarah', 1983);
+
+sarah.calcAge();
+
+/* 
+1. Re-create challenge 1, but this time using an ES6 class;
+2. Add a getter called 'speedUS' which returns the current speed in mi/h (divide by 1.6);
+3. Add a setter called 'speedUS' which sets the current speed in mi/h (but converts it to km/h before storing the value, by multiplying the input by 1.6);
+4. Create a new car and experiment with the accelerate and brake methods, and with the getter and setter.
+
+DATA CAR 1: 'Ford' going at 120 km/h
+
+GOOD LUCK 😀
+*/
+
+const carSpeed = class {
+  constructor(make, carSpeed) {
+    this.make = make;
+    this._carSpeed = carSpeed;
+  }
+
+  get speedUS() {
+    return this._carSpeed / 1.6;
+  }
+
+  set speedUS(speed) {
+    this._carSpeed = speed * 1.6;
+  }
+
+  accelerate() {
+    this._carSpeed += 10;
+    console.log(`Accelerated! New speed: ${this._carSpeed} km/h`);
+  }
+
+  brake() {
+    this._carSpeed -= 5;
+    console.log(`Brake applied! New speed: ${this._carSpeed} km/h`);
+  }
+};
+
+const car = new carSpeed('bmw', 120);
+console.log(car);
+
+//console.log(car.speedUS);
+
+car.speedUS = 120;
+console.log(car);
